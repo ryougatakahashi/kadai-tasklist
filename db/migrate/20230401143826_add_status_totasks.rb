d39c4b9509ee5e670a495475b23c826b6f7930aa
ryouga takahashi <ryouga201215@yahoo.co.jp>
2023-04-01 14:43:50 +0000

    DRY

---
+++ db/migrate/20230401143826_add_status_totasks.rb
@@ -0,0 +1,4 @@
+class AddStatusTotasks < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_01_054046) do
+ActiveRecord::Schema.define(version: 2023_04_01_143826) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
