4374b8924b7a0914cfe2494fb58fd7f345ec1e68
ryouga takahashi <ryouga201215@yahoo.co.jp>
2023-04-02 11:13:53 +0000

    up

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2023_04_01_143826) do
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
